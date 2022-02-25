dda3e23c33cc502271044f2af12c399694faa368
Ricardo Gonçalves <32261260+RicardoALFCG@users.noreply.github.com>
2022-02-25 00:13:45 +0000

    folder struct updated

---
+++ XMC_Relax_Kit_Projects/Devices_integration_example/xmc4200_platform2go/NewHavenDisplay/Debug/Dave/Generated/TIMER/timer.d
@@ -0,0 +1,62 @@
+Dave/Generated/TIMER/timer.o \
+ Dave/Generated/TIMER/timer.d Dave/Generated/TIMER/timer.o: \
+ ../Dave/Generated/TIMER/timer.c ../Dave/Generated/TIMER/timer.h \
+ ../Dave/Generated/TIMER/timer_conf.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Dave/Generated/GLOBAL_CCU4/global_ccu4.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc_ccu4.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc_common.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc_device.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Infineon/XMC4200_series/Include/XMC4200.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/core_cm4.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/cmsis_version.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/mpu_armv7.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Infineon/XMC4200_series/Include/system_XMC4200.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc4_ccu4_map.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc_scu.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc4_scu.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Dave/Generated/DAVE_Common.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Dave/Generated/GLOBAL_CCU4/global_ccu4_conf.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Dave/Generated/GLOBAL_CCU4/global_ccu4_extern.h \
+ ../Dave/Generated/TIMER/timer_extern.h
+
+../Dave/Generated/TIMER/timer.h:
+
+../Dave/Generated/TIMER/timer_conf.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Dave/Generated/GLOBAL_CCU4/global_ccu4.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc_ccu4.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc_common.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc_device.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Infineon/XMC4200_series/Include/XMC4200.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/core_cm4.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/cmsis_version.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/mpu_armv7.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Infineon/XMC4200_series/Include/system_XMC4200.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc4_ccu4_map.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc_scu.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc4_scu.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Dave/Generated/DAVE_Common.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Dave/Generated/GLOBAL_CCU4/global_ccu4_conf.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Dave/Generated/GLOBAL_CCU4/global_ccu4_extern.h:
+
+../Dave/Generated/TIMER/timer_extern.h:
---
+++ XMC_Relax_Kit_Projects/Devices_integration_example/xmc4200_platform2go/NewHavenDisplay/Debug/Libraries/XMCLib/src/xmc_dsd.d
@@ -0,0 +1,33 @@
+Libraries/XMCLib/src/xmc_dsd.o \
+ Libraries/XMCLib/src/xmc_dsd.d Libraries/XMCLib/src/xmc_dsd.o: \
+ ../Libraries/XMCLib/src/xmc_dsd.c \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc_dsd.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc_common.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc_device.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Infineon/XMC4200_series/Include/XMC4200.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/core_cm4.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/cmsis_version.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/mpu_armv7.h \
+ C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Infineon/XMC4200_series/Include/system_XMC4200.h
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc_dsd.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc_common.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/XMCLib/inc/xmc_device.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Infineon/XMC4200_series/Include/XMC4200.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/core_cm4.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/cmsis_version.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Include/mpu_armv7.h:
+
+C:/Users/RGoncalves/Documents/GitHub/uControllers/XMC_Relax_Kit_Projects/APPs_Implementation_Examples/xmc4200_platform2go/NewHavenDisplay/Libraries/CMSIS/Infineon/XMC4200_series/Include/system_XMC4200.h:
